bb0113653d22db813eb19002f2201d148ec39e25
PressStart10 <iltsn29@sudburystudents.org>
2021-02-19 21:02:48 +0000

    .

--- main.cs
+++ main.cs
@@ -1,19 +1,33 @@
 using System;
 using System.IO;
+using System.Collections.Generic;
 class MainClass {
-	public static string RemoveCOMMAND(string text) { text = text.Replace(".filesystem !CREATETEXT. ", "").Replace(".filesystem !CODE_12. ",""); var oRegEx = new System.Text.RegularExpressions.Regex("<[^>]+>"); return oRegEx.Replace(text, string.Empty); }
+	Dictionary<string, string> varstorage=
+				new Dictionary<string, string>();
+	string variable;
+	public static string RemoveCOMMAND(string text) { text = text.Replace(".variablestring !SET. ", "").Replace(".filesystem !CREATETEXT. ", "").Replace(".filesystem !CODE_12. ",""); var oRegEx = new System.Text.RegularExpressions.Regex("<[^>]+>"); return oRegEx.Replace(text, string.Empty); }
 	static void loop() {
 		ren();
 	}
 	static void ren() {
+		var variable = "";
 		Console.Write("litsystem.shell_input$ ");
 		string et = Console.ReadLine();
+		if(et.Contains(".variablestring !SET.")){
+			variable = RemoveCOMMAND(et);
+			varstorage.Add(variable, "orange");
+			System.Random random = new System.Random(); 
+			int rtt = random.Next(50);
+			Console.WriteLine("litsystem_output_code:"+rtt+".litsystem_output: new var \""+variable+"\" set");
+			loop();
+		}
 		if(et.Contains(".filesystem !CREATETEXT. ")){
 			string etr = RemoveCOMMAND(et);
 			System.Random random = new System.Random(); 
 			int rtt = random.Next(50);
 			Console.WriteLine("litsystem_output_code:"+rtt+".litsystem_output:"+etr);
 			loop();
+			
 		}
 		if(et == ".filesystem !CODE_12."){
 			System.Random random = new System.Random(); 
@@ -22,7 +36,7 @@ class MainClass {
 			System.Environment.Exit(12);
 		}
 		if(et == ".help."){
-			Console.WriteLine("command list and uses: \n .filesystem !CREATETEXT.{prints a string input} \n .filesystem !CODE_12.{ exits the program whith exit code (12) }\n litsystem output: \n litsystem_output_code: { prints the output code } \n litsystem_output:{ shows the output } \n litsystem.shell_input$ { command line input }");
+			Console.WriteLine("command list and uses: \n .filesystem !CREATETEXT.{prints a string input} \n .filesystem !CODE_12.{ exits the program whith exit code (12) }\nlitsystem output: \n litsystem_output_code: { prints the output code } \n litsystem_output:{ shows the output } \n litsystem.shell_input$ { command line input }");
 			loop();
 		}
 		else{
