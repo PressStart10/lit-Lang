20548182bc928533f1e672aa1553ad9103ee2931
PressStart10 <iltsn29@sudburystudents.org>
2021-02-26 21:27:59 +0000

    simplified & added variable.edit(edits vars) + variable.get(prints vars)

--- main.cs
+++ main.cs
@@ -9,7 +9,7 @@ class Seprite{
 	}
 class MainClass {
 	static Seprite s = new Seprite();
-	public static string RemoveCOMMAND(string text) { text = text.Replace(".variablestring !SET. ", "").Replace(".variablestring !EDIT. ", "").Replace(".filesystem !CREATETEXT. ", "").Replace(".filesystem !CODE_12. ",""); var oRegEx = new System.Text.RegularExpressions.Regex("<[^>]+>"); return oRegEx.Replace(text, string.Empty); }
+	public static string RemoveCOMMAND(string text) { text = text.Replace("variable.get ", "").Replace("variable.new ", "").Replace("variable.edit ", "").Replace("sys.print ", "").Replace("sys.exit12",""); var oRegEx = new System.Text.RegularExpressions.Regex("<[^>]+>"); return oRegEx.Replace(text, string.Empty); }
 	static void loop() {
 		ren();
 	}
@@ -17,15 +17,34 @@ class MainClass {
 		var variable = "";
 		Console.Write("litsystem.shell_input$ ");
 		string et = Console.ReadLine();
-		if(et.Contains(".variablestring !SET.")){
+		if(et.Contains("variable.get")){
+			string e = RemoveCOMMAND(et);
+			if(s.varstorage[e] == ""){
+				Console.WriteLine("[text blank]");
+			}
+			else{
+				Console.WriteLine(s.varstorage[e]);
+			}
+		}
+		if(et.Contains("variable.edit")){
+			string y_tho = RemoveCOMMAND(et);
+			Console.Write("set variable "+ y_tho +" to:");
+			string ytho = Console.ReadLine();
+			s.varstorage[y_tho] = ytho;
+			System.Random random = new System.Random(); 
+			int rtt = random.Next(50);
+			Console.WriteLine("litsystem_output_code:"+rtt+".litsystem_output: var \""+y_tho+"\" set to: "+ytho);
+			loop();
+		}
+		if(et.Contains("variable.new")){
 			variable = RemoveCOMMAND(et);
-			s.varstorage.Add(variable, "orange");
+			s.varstorage.Add(variable, "");
 			System.Random random = new System.Random(); 
 			int rtt = random.Next(50);
 			Console.WriteLine("litsystem_output_code:"+rtt+".litsystem_output: new var \""+variable+"\" set");
 			loop();
 		}
-		if(et.Contains(".filesystem !CREATETEXT. ")){
+		if(et.Contains("sys.print ")){
 			string etr = RemoveCOMMAND(et);
 			System.Random random = new System.Random(); 
 			int rtt = random.Next(50);
@@ -33,14 +52,14 @@ class MainClass {
 			loop();
 			
 		}
-		if(et == ".filesystem !CODE_12."){
+		if(et == "sys.exit12"){
 			System.Random random = new System.Random(); 
 			int rtt = random.Next(50);
 			Console.WriteLine("litsystem_output_code:"+rtt);
 			System.Environment.Exit(12);
 		}
 		if(et == ".help."){
-			Console.WriteLine("command list and uses: \n .filesystem !CREATETEXT.{prints a string input} \n .filesystem !CODE_12.{ exits the program whith exit code (12) }\nlitsystem output: \n litsystem_output_code: { prints the output code } \n litsystem_output:{ shows the output } \n litsystem.shell_input$ { command line input }");
+			Console.WriteLine("command list and uses: \n sys.print{prints a string input} \n sys.exit12{ exits the program whith exit code (12) }\n variable.new {adds a variable}\n variable.edit {edits a variable}\n variable.get{prints a variable}\nlitsystem output: \n litsystem_output_code: { prints the output code } \n litsystem_output:{ shows the output } \n litsystem.shell_input$ { command line input }");
 			loop();
 		}
 		else{
